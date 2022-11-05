e31b8b0d253b668768d8bf5fc6604f38b881fb84
lazlanrafar <lazlanrafar@gmail.com>
2022-11-05 21:33:26 +0700

    setup jenis pkm landing page

--- resources/views/pages/landing-page/index.blade.php
+++ resources/views/pages/landing-page/index.blade.php
@@ -53,101 +53,9 @@
 
     <main class="">
         @include('pages.landing-page.banner')
+        @include('pages.landing-page.jenis-pkm')
 
-        <section style="background-color: #373e46">
-            <div class="container py-3">
-                <h4 class="text-center text-white">
-                    JENIS PKM YANG DI LOMBAKAN
-                </h4>
-                <p class="text-center text-white">Terdapat 8 Bidang</p>
-                <div class="row">
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-ai.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-AI</h5>
 
-                                <p class="mb-0">(Artikel Ilmiah)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-gft.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-GFT</h5>
-
-                                <p class="mb-0">
-                                    (Gagasan Futuristik Tertulis)
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-k1.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-K</h5>
-
-                                <p class="mb-0">(Kewirausahaan)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-kc.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-KC</h5>
-
-                                <p class="mb-0">(Karsa Cipta)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-ki1.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-KI</h5>
-
-                                <p class="mb-0">(Karya Inovatif)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-pi1.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-PI</h5>
-
-                                <p class="mb-0">(Penerapan IPTEK)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-pm1.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-PM</h5>
-
-                                <p class="mb-0">(Pengabdian Masyarakat)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/image/icon-pkm-vgk1.svg" height="65" alt="" class="mb-2" />
-                                <h5 class="mb-0">PKM-VGK</h5>
-
-                                <p class="mb-0">
-                                    (Video Gagasan Futuristik)
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
 
         <section>
             <div class="container py-3">
@@ -157,8 +65,7 @@
                     <div class="col-4 mb-4">
                         <div class="">
                             <div class="text-center">
-                                <img src="/image/gambar-sosialiasi.png" height="140" alt=""
-                                    class="mb-2" />
+                                <img src="/image/gambar-sosialiasi.png" height="140" alt="" class="mb-2" />
                                 <h5 class="mb-0">1.Sosialiasi</h5>
 
                                 <p class="mb-0">
---
+++ resources/views/pages/landing-page/jenis-pkm.blade.php
@@ -0,0 +1,96 @@
+<section style="background-color: #373e46">
+    <div class="container py-5">
+        <div class="mb-5">
+            <h4 class="text-center text-white">
+                JENIS PKM YANG DI LOMBAKAN
+            </h4>
+            <p class="text-center text-white">Terdapat 8 Bidang</p>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-ai.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-AI</h5>
+
+                        <p class="mb-0">(Artikel Ilmiah)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-gft.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-GFT</h5>
+
+                        <p class="mb-0">
+                            (Gagasan Futuristik Tertulis)
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-k1.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-K</h5>
+
+                        <p class="mb-0">(Kewirausahaan)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-kc.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-KC</h5>
+
+                        <p class="mb-0">(Karsa Cipta)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-ki1.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-KI</h5>
+
+                        <p class="mb-0">(Karya Inovatif)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-pi1.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-PI</h5>
+
+                        <p class="mb-0">(Penerapan IPTEK)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-pm1.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-PM</h5>
+
+                        <p class="mb-0">(Pengabdian Masyarakat)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/image/icon-pkm-vgk1.svg" height="65" alt="" class="mb-2" />
+                        <h5 class="mb-0">PKM-VGK</h5>
+
+                        <p class="mb-0">
+                            (Video Gagasan Futuristik)
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
